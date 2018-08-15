509b94caa1e99cf6dedf7b003b1d20f7b41a02a8
ToFut <segevhalfon1@gmail.com>
2018-08-15 23:13:24 +0300

    and..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  toSupplyMe() {
+    location.href = 'https://app.supplyme.net';
+  }
+  call() {
+    location.href = 'tel:0508875140';
+  }
+  shareApp() {
+    let WhatsAppMesage = '';
+    WhatsAppMesage = ' https://api.whatsapp.com/send?text=SupplyME ' +
+      ' אפליקציה לניהול ספקים באופן חכם מהיר יעיל תוך חיסכון משמעותי בהוצאות ' +
+      ' נסה עכשיו בחינם !' + ' %0A ' + ' https://supplyme.net ';
+    location.href = WhatsAppMesage;
+
+
+  }
+
 }
